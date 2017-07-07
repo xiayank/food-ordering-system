2a2ce21630cad6084055e09caf1809b1894e7fdc
Yan Xia <yan.xia.cs@gmail.com>
2017-07-07 16:44:25 +0800

    add repository method of restaurant service

--- restaurant-service/src/main/java/com/nic/repository/menuItemRepository.java
+++ restaurant-service/src/main/java/com/nic/repository/menuItemRepository.java
@@ -1,7 +1,18 @@
 package com.nic.repository;
 
+import com.nic.model.MenuItem;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
 /**
  * Created by NIC on 7/7/17.
  */
-public interface menuItemRepository {
+@RepositoryRestResource(collectionResourceRel = "menuItems")
+public interface menuItemRepository extends PagingAndSortingRepository<MenuItem, String>{
+    @RestResource(rel = "byRestaurantId")
+    public List<MenuItem> findAllByRestaurantId(@Param("restaurantId") String restaurantId);
 }
--- restaurant-service/src/main/java/com/nic/repository/restaurantRepository.java
+++ restaurant-service/src/main/java/com/nic/repository/restaurantRepository.java
@@ -3,6 +3,7 @@ package com.nic.repository;
 import com.nic.model.Restaurant;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -11,6 +12,6 @@ import org.springframework.data.rest.core.annotation.RestResource;
  */
 @RepositoryRestResource(path = "restaurants", collectionResourceRel= "restaurants")
 public interface restaurantRepository extends PagingAndSortingRepository<Restaurant, String>{
-    @RestResource(rel = "byName")
+    @RestResource(rel = "byName", description = @Description("Find by name"))
     public Restaurant findFirstByName(@Param("name") String name);
 }
