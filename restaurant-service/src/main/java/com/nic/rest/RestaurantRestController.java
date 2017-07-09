ae402ea173576037465acfbafa494fd1d95e0a92
Yan Xia <yan.xia.cs@gmail.com>
2017-07-09 10:50:11 +0800

    add  rest controller of restaurant service

---
+++ restaurant-service/src/main/java/com/nic/rest/RestaurantRestController.java
@@ -0,0 +1,23 @@
+package com.nic.rest;
+
+import com.nic.service.MenuItemService;
+import com.nic.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by NIC on 7/9/17.
+ */
+@RestController
+public class RestaurantRestController {
+    private RestaurantService restaurantService;
+    private MenuItemService menuItemService;
+
+    @Autowired
+
+    public RestaurantRestController(RestaurantService restaurantService, MenuItemService menuItemService){
+        this.restaurantService = restaurantService;
+        this.menuItemService = menuItemService;
+    }
+
+}
