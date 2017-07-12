4db513be2a2fff18f661bca2b6d9681ae47ee554
Yan Xia <yan.xia.cs@gmail.com>
2017-07-12 16:12:22 +0800

    add repository of order service

--- restaurant-service/src/main/java/com/nic/repository/menuItemRepository.java
+++ restaurant-service/src/main/java/com/nic/repository/menuItemRepository.java
@@ -14,6 +14,6 @@ import java.util.List;
 @RepositoryRestResource(collectionResourceRel = "menuItems")
 public interface MenuItemRepository extends PagingAndSortingRepository<MenuItem, String>{
     @RestResource(rel = "byRestaurantId")
-    public List<MenuItem> findAllByRestaurantId(@Param("restaurantId") String restaurantId);
+    List<MenuItem> findAllByRestaurantId(@Param("restaurantId") String restaurantId);
 
 }
