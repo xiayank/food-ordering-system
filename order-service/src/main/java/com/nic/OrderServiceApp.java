c3eed4cf28191c6881dab038fc4ff44f7c2cf16d
Yan Xia <yan.xia.cs@gmail.com>
2017-07-12 15:30:34 +0800

    add module order service

---
+++ order-service/src/main/java/com/nic/OrderServiceApp.java
@@ -0,0 +1,14 @@
+package com.nic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by NIC on 7/12/17.
+ */
+@SpringBootApplication
+public class OrderServiceApp {
+    public static void main(String[] args){
+        SpringApplication.run(OrderServiceApp.class, args);
+    }
+}
